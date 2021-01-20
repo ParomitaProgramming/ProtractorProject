3d1bb02eecae35077144ab5a6c56f231ff12660a
Sayan Dasgupta <57595408+SayanDasguptaAutomation@users.noreply.github.com>
2021-01-20 19:56:29 +0530

    Update

--- Tests/Calculator.js
+++ Tests/Calculator.js
@@ -1,13 +1,28 @@
-describe('Test_Case_1', function(){
+describe('Calculator', function(){
     var CalculatorPage = require('../Tests/CalculatorPage.js')
-    it('Test_Case_1', function(){
+
+    it('Addition', function(){
 
         browser.get('http://www.way2automation.com/angularjs-protractor/calc/');
 
         CalculatorPage.enterFirstNumber('6')
+        CalculatorPage.doAddition();
         CalculatorPage.enterSecondNumber('2');
         CalculatorPage.clickSubmitButton();
-        CalculatorPage.verifyOutput('8')
+        CalculatorPage.verifyOutput('8') 
+
+    });
+
+    it('Substraction', function(){
+
+        browser.get('http://www.way2automation.com/angularjs-protractor/calc/');
+
+        CalculatorPage.enterFirstNumber('6')
+        CalculatorPage.doSubstraction();
+        CalculatorPage.enterSecondNumber('2');
+        CalculatorPage.clickSubmitButton();
+        CalculatorPage.verifyOutput('5') 
+
     });
 
 });
--- Tests/CalculatorPage.js
+++ Tests/CalculatorPage.js
@@ -4,6 +4,7 @@ var CalculatorPage = function(){
     var secondNumber = element(by.model('second'));
     var submitButton = element(by.id('gobutton'));
     var outputText = element(by.xpath("//*[@class='ng-binding']"));
+    var selection = element(by.model('operator'));
 
     this.enterFirstNumber = function(firstNo){
         firstNumber.sendKeys(firstNo);
@@ -19,6 +20,26 @@ var CalculatorPage = function(){
 
     this.verifyOutput = function(ExpectedValue){
         expect(outputText.getText()).toEqual(ExpectedValue);
+    };
+
+    this.doAddition = function(){
+        selection.$('[value="ADDITION"]').click();
+    };
+
+    this.doSubstraction = function(){
+        selection.$('[value="SUBTRACTION"]').click();
+    };
+
+    this.doMultiply = function(){
+        selection.$('[value="MULTIPLICATION"]').click();
+    };
+
+    this.doDivision = function(){
+        selection.$('[value="DIVISION"]').click();
+    };
+
+    this.doModulo = function(){
+        selection.$('[value="MODULO"]').click();
     };
 };
 
